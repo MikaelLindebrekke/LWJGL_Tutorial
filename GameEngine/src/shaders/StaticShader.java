c57793182a20c4381536ef41efb62e45e68cd7e4
Mikael <Mikael@192.168.0.6>
2021-03-08 12:13:40 +0100

    Session 3

--- GameEngine/src/shaders/StaticShader.java
+++ GameEngine/src/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 	
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.glsl";
 	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.glsl";
+	
+	private int location_transformationMatrix;
 
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -15,4 +19,12 @@ public class StaticShader extends ShaderProgram {
 		super.bindAttribute(1, "textureCoords");
 	}
 
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
 }
